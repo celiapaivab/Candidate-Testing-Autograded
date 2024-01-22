e9682fd2480c46428b54d4238e53708d9aa13a24
celiapaivab <celiadepaivabruno@gmail.com>
2024-01-21 18:53:13 -0600

    Part 2 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,39 +11,40 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3" ];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  const input = require('readline-sync');
 
   candidateName = input.question("Enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  const input = require('readline-sync');
 
-  candidateAnswer = input.question("Who was the first American woman in space? ");
+  for (i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz() {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-if (candidateAnswer == correctAnswer) {
-  console.log("Answer is correct!")
-} else {
-  console.log("Answer is not correct.")
-}
+  for (i = 0; i < correctAnswers.length; i++) {
+    if (candidateAnswers[i] == correctAnswers[i]) {
+      console.log(`The answer "${candidateAnswers[i]}" is correct!`)
+    } else {
+      console.log(`The answer "${candidateAnswers[i]}" is not correct. Correct answer is ""${correctAnswers[i]}""`)
+    }
+  }
+    let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
-
-  return grade;
+    return grade;
 }
 
 function runProgram() {
